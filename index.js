506411f81c1e568662c01ce5a54af1dc08947e48
allenWTF <niowen@mail.ru>
2016-10-15 10:02:42 +0300

    Some functions were added to Server.js

--- index.js
+++ index.js
@@ -6,4 +6,15 @@ const web = require('webbox');
 // var wb = new webbox.Builder.Box();
 var platform = new web.Server.Box();
 
+
 platform.createServer(1337);
+platform.newStaticPage('/test','/html/SendBox.html');
+
+/*platform.newRequestListener('/test', "post", function (req,res) {
+    res.send(req.body.str);
+});*/
+
+platform.newRequestListener('/test', "post", function (req,res) {
+    var json_obj = {name: "alex", lastname: "belov"};
+    platform.sendJSON(res, json_obj);
+});
